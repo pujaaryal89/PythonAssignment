cc438f53aabbbb07a1cf95cdb634b29cfa5fa027
Puja <aryalpooja89@gmail.com>
2020-07-04 12:50:12 +0545

    function

--- function/function10.py
+++ function/function10.py
@@ -7,14 +7,6 @@ even_list = get_even_li(sample_li)
 
 print(f'Sample list: {sample_li}')
 print(f'Even list: {even_list}')
-
-
-
-# list=[1,2,3,4]
-# for n in list:
-#     if n % 2==0:
-#         num=[n]
-# print(num)
         
         
         
